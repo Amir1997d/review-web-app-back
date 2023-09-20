6021e72d8ccf1bf79ebc60af5ba7d4982531128e
Amir Dastouri <97881149+Amir1997d@users.noreply.github.com>
2023-09-20 16:51:08 +0300

    Update db.js

--- controllers/db.js
+++ controllers/db.js
@@ -1,10 +1,12 @@
 const { Sequelize, DataTypes } = require('sequelize');
 
-const sequelize = new Sequelize('reviewapp', 'reviewapp_user', 'Hi0ZDMC841xscmFYxtDgvB8c2Hxa1bKo', {
+const sequelize = new Sequelize('reviewapp', {
     host: 'dpg-ck5f1eei9prc73anc8s0-a',
     database: 'reviewapp',
     port: 5432,
     dialect: 'postgres',
+    user: 'reviewapp_user',
+    password: 'Hi0ZDMC841xscmFYxtDgvB8c2Hxa1bKo',
 });
 
 sequelize.sync();
