27f09c08ad82c9489563e19015af75d321678431
Amir Dastouri <97881149+Amir1997d@users.noreply.github.com>
2023-09-20 16:24:03 +0300

    Update sequelize connection properties in db.js

--- controllers/db.js
+++ controllers/db.js
@@ -1,7 +1,9 @@
 const { Sequelize, DataTypes } = require('sequelize');
 
-const sequelize = new Sequelize('reviewapp', '', '', {
-    host: 'localhost',
+const sequelize = new Sequelize('reviewapp', 'reviewapp_user', 'Hi0ZDMC841xscmFYxtDgvB8c2Hxa1bKo', {
+    host: 'dpg-ck5f1eei9prc73anc8s0-a',
+    database: 'reviewapp',
+    port: 5432,
     dialect: 'postgres',
 });
 
