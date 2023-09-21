fe774f5c17bccc234bff5c88cca619d41390a9a2
Amir Dastouri <97881149+Amir1997d@users.noreply.github.com>
2023-09-21 17:54:05 +0300

    Update server.js

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ app.use(
     cors({
         // origin: "http://localhost:3000",
         // origin: "https://reviewer-6vyp.onrender.com",
-        origin: "http://press-brake.ir:3000",
+        origin: "185.220.205.86:3000",
         methods: "GET,POST,PUT,DELETE",
         credentials: true,
     })
